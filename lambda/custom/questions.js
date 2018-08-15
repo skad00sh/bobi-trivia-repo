4d79e12ed753cf6385b9901f63c37a77a4b8ecd7
skad00sh <heramblonkar@gmail.com>
2018-08-15 17:17:14 +0530

    Added all required files

---
+++ lambda/custom/questions.js
@@ -0,0 +1,89 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_IN: [
+    {
+      'What is the first letter in the surname of Bobi?': [
+        'K',
+        'C',
+        'L',
+        'F',
+        'S',
+        'X',
+      ],
+    },
+    {
+      'What is the initial letter in the name of Bobi?': [
+        'S',
+        'T',
+        'U',
+        'H',
+        'K',
+        'M',
+      ],
+    },
+    {
+      'What was the name of the first kernel cooked by Bobi?': [
+        ' \'Phantom\' The overclocked lord',
+        ' \'Clean and Tweaked\' which is not a kernel',
+        ' \'Franco\' of course not',
+        ' \'Thunder Zip\' Because zap is already copyrighted',
+        ' \'Kernel by Chinki\' wait is that even a thing',
+        ],
+    },
+    {
+      'Which of the following epic phone Bobi had?': [
+        'Xperia L with brand new camera ring',
+        'Xiaomi Redmi Note 3 the barbeque maker',
+        'Redmi Note 4 the note 3 reborn',
+        'Mi with some model number and beauty spot',
+        'Pixel 2 good game well played',
+        'iPhone X no comments',
+      ],
+    }
+    {
+      'What is the favourite animal of Bobi?': [
+        'Cow',
+        'Bull',
+        'Snake',
+        'Mouse',
+        'Elephant',
+        'Pig',
+      ],
+    },
+    {
+      'What is the second most famous adjective of Bobi?': [
+        'The Inglorious',
+        'Bald',
+        'Fat',
+        'Tall',
+        'Geeky',
+        'Pro',
+      ],
+    },
+    {
+      'Why Bobi had no other option left? Due to lack of space in': [
+        'Room',
+        'Pen drive',
+        'Hard drive',
+        'Car',
+        'Pocket',
+        'Laptop',
+      ],
+    },
+    {
+      'Bobi is also knows as': [
+        'Geeky Bobi',
+        'Techy Bobi',
+        'Cyber Bobi',
+        'Bobi Guruji',
+        'Bobi HD',
+        'Bobi Therapy',
+      ],
+    },
+  ],
+};
